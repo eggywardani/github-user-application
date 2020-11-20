23811653c2cb8d220821aae2eabe5b31f847634f
eggywardani <eggyandikawardani@gmail.com>
2020-11-20 10:40:25 +0700

    clean code contants variable

--- consumerapp/src/main/java/com/eggy/consumerapp/data/Constants.kt
+++ consumerapp/src/main/java/com/eggy/consumerapp/data/Constants.kt
@@ -2,7 +2,7 @@ package com.eggy.consumerapp.data
 
 object Constants {
 
-    const val API_KEY = "// YOUR API KEY"
+    const val API_KEY = "//YOUR API KEY"
     const val BASE_URL = "https://api.github.com/"
 
 }
